572c6b6356e3ddd7e19fbe26b2ae6d86261a32cf
Robert Bubnik <rbubnik@gmail.com>
2024-08-20 20:43:08 -0400

    passing

---
+++ index.js
@@ -0,0 +1,35 @@
+function  preserveDecimal (n ) {
+    return parseFloat(n)
+}
+
+function makeInt (n) {
+ return parseInt(n, 10)
+
+
+}
+
+function add (a,b) {
+    return a+b
+}
+
+function subtract (a,b) {
+    return a-b
+}
+
+function multiply (a,b) {
+    return a*b
+}
+
+function divide(a,b) {
+    return a/b
+}
+
+function decrement (a) {
+    const b = --a
+    return b
+}
+
+function increment (a) {
+    const b = ++a
+    return b
+}
